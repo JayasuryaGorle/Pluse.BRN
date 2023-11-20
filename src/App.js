35d984a15c9fd26c8adf1641c9f58e7db589e949
Jaya Surya G <jayasuryagorle@gmail.com>
2023-11-20 13:55:13 +0530

    added the components files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 
 function App() {
   return (
     <div className="App">
-
+<Signup/>
+<Login/>
+<Home/>
 
     </div>
   );
